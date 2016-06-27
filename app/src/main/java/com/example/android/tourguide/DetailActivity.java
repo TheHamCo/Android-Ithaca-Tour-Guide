8225555026bc838fa4bbeada272b04618c10e12d
Michael Dijamco <mdd233@cornell.edu>
2016-06-27 19:14:42 -0400

    feat: Init Google Maps intent

--- app/src/main/java/com/example/android/tourguide/DetailActivity.java
+++ app/src/main/java/com/example/android/tourguide/DetailActivity.java
@@ -1,8 +1,11 @@
 package com.example.android.tourguide;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -19,6 +22,19 @@ public class DetailActivity extends AppCompatActivity {
 
         locationName.setText(detailIntent.getStringExtra("location_name"));
         locationDesc.setText(detailIntent.getStringExtra("location_desc"));
+
+        Button goToMap = (Button)findViewById(R.id.goto_map);
+        if (goToMap != null) {
+            goToMap.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                    Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4192?z=20?q=37.7749,-122.4192(Pin)");
+                    Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4192?q=37.7749,-122.4192(Pin)");
+                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                    startActivity(mapIntent);
+                }
+            });
+        }
 
     }
 }
