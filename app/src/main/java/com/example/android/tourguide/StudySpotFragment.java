a7c4f700765fdc1e25150512d6237472565c5d3c
Michael Dijamco <mdd233@cornell.edu>
2016-06-27 19:34:08 -0400

    feat: Add placeholder list data for all fragments

--- app/src/main/java/com/example/android/tourguide/StudySpotFragment.java
+++ app/src/main/java/com/example/android/tourguide/StudySpotFragment.java
@@ -1,10 +1,16 @@
 package com.example.android.tourguide;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by mdd23 on 6/27/2016.
@@ -18,6 +24,27 @@ public class StudySpotFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.location_list, container, false);
+
+        final ArrayList<Location> locations = new ArrayList<Location>();
+        locations.addAll(Arrays.asList(
+                new Location("McGraw Clocktower", "description", 42.4475905, -76.4850102)
+                ,new Location("Ho Plaza", "description")
+                ,new Location("The Slope", "description")
+        ));
+        LocationAdapter locationAdapter = new LocationAdapter(getActivity(), locations);
+        ListView listView = (ListView)rootView.findViewById(R.id.list);
+        listView.setAdapter(locationAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Location currentLocation = locations.get(position);
+                Intent detailIntent = new Intent(getActivity(), DetailActivity.class);
+                detailIntent.putExtra("location_name", currentLocation.getmLocationName());
+                detailIntent.putExtra("location_desc", currentLocation.getmLocationDescription());
+                detailIntent.putExtra("location_geoURI", currentLocation.getGMapsURI());
+                startActivity(detailIntent);
+            }
+        });
         return rootView;
     }
 }
