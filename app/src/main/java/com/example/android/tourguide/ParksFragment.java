a8e908bf597f79e7769e55bbdba2ab6bf8bc4e50
Michael Dijamco <mdd233@cornell.edu>
2016-06-27 20:35:21 -0400

    feat: Add Park coordinates

--- app/src/main/java/com/example/android/tourguide/ParksFragment.java
+++ app/src/main/java/com/example/android/tourguide/ParksFragment.java
@@ -27,17 +27,15 @@ public class ParksFragment extends Fragment{
 
         final ArrayList<Location> locations = new ArrayList<Location>();
         locations.addAll(Arrays.asList(
-                 new Location("Cayuga Lake","description")
-                ,new Location("Robert H. Treman State Park", "description", 42.4475905, -76.4850102)
-                ,new Location("Taughannock Falls State Park","description")
-                ,new Location("Buttermilk Falls State Park","description")
-                ,new Location("Cayuga Waterfront Trail","description")
-                ,new Location("Triphammer Falls","description")
-                ,new Location("Potter's Falls on Six Mile Creek","description")
-                ,new Location("Ithaca Falls on Fall Creek","description")
-                ,new Location("Cascadilla Falls","description")
-                ,new Location("Stewart Park","description")
-                ,new Location("Greek Peak Mountain Resort","description")
+                 new Location("Cayuga Lake","description",42.6776693, -76.6990172)
+                ,new Location("Robert H. Treman State Park", "description", 42.3989104, -76.5680964)
+                ,new Location("Taughannock Falls State Park","description",42.5361329, -76.6115596)
+                ,new Location("Buttermilk Falls State Park","description",42.4192149, -76.5143186)
+                ,new Location("Cayuga Waterfront Trail","description",42.4489922, -76.5140779)
+                ,new Location("Triphammer Falls","description",42.451580, -76.480381)
+                ,new Location("Ithaca Falls on Fall Creek","description",42.452782, -76.491660)
+                ,new Location("Stewart Park","description",42.461209, -76.503880)
+                ,new Location("Greek Peak Mountain Resort","description",42.5123724, -76.1475518)
         ));
         LocationAdapter locationAdapter = new LocationAdapter(getActivity(), locations);
         ListView listView = (ListView)rootView.findViewById(R.id.list);
